039a2c2fc21457dcced5dbad0498da08bca86a83
MostafaShelim <mostafarmstu@gmail.com>
2021-05-07 16:59:50 +0600

    local storage done

--- script.js
+++ script.js
@@ -15,55 +15,53 @@ let usrAuthor = document.querySelector('#author-name');
 let usrPage = document.querySelector('#pages');;
 let btn = document.querySelector('#submit');
 let cards = document.querySelector('.cards');
-let read = document.querySelectorAll('.button');
-let removeBtns = [];
-let id = 0;
+let yes = document.querySelectorAll("button[value='yes']");
+let no = document.querySelector("button[value ='no']");
+let read = document.querySelectorAll('.button')
 let yesNo;
-
-// console.log(btn);
+function restore(){
+    let items = localStorage.getItem('myBook');
+    items = JSON.parse(items);
+    for(let i = 0; i < items.length; i++){
+        createElement(items[i]);
+        books.push(items[i]);
+    }
+}
 
 //create all card element of a book
-function createElement(yesNo){
+function createElement(book){
     let card = document.createElement('div');
     card.className = 'card';
 
     let title = document.createElement('h2');
-    title.innerText = "Title: " + usrTitle.value;
-    usrTitle.value="";
+    title.innerText = "Title: " + book.title;
 
     let authorName = document.createElement('h3');
-    authorName.innerText = "Author: " + usrAuthor.value;
-    usrAuthor.value = "";
+    authorName.innerText = "Author: " + book.author;
 
     let pageNumber = document.createElement('h3');
-    pageNumber.innerText = "Pages: " + usrPage.value;
-    usrPage.value = "";
+    pageNumber.innerText = "Pages: " + book.page;
 
-    let read = document.createElement('h3');
-    read.innerText = "Read: " + yesNo.innerHTML;
-    yesNo.style.background = "";
-    yesNo.style.color = "";
-    yesNo.innerHTML.value = "";
+    let read = document.createElement('p');
+    read.innerText = "Read: " + book.status;
 
-    let btnRemove = document.createElement('button');
-    btnRemove.className = "btnRemove";
-    btnRemove.innerText = "Remove";
-    btnRemove.value = id++;
-    removeBtns.push(btnRemove);
-
-    formStructur(title, authorName, pageNumber, read, btnRemove, card);
+    // let btnRemove = document.createElement('button');
+    // btnRemove.className = "btnRemove";
+    // btnRemove.innerText = "Remove";
+    // removeBtns.push(btnRemove);
+    // console.log(title, authorName, pageNumber,read,card);
+    formStructur(title, authorName, pageNumber, read, card);
 
     //remove book from book-self
-    removeBook(removeBtns);
 }
 
 // new div under book-info
-function formStructur(title, authorName, pageNumber, read, btn, card){
+function formStructur(title, authorName, pageNumber, read, card){
     card.appendChild(title);
     card.appendChild(authorName);
     card.appendChild(pageNumber);
     card.appendChild(read);
-    card.appendChild(btn)
+    // card.appendChild(btn)
     cards.appendChild(card);
 }
 
@@ -93,9 +91,15 @@ btn.addEventListener('click', (e)=>{
     if(usrPage.value && usrAuthor.value  && usrTitle.value){
         const newBook = new Book(usrTitle, usrAuthor, usrPage, yesNo.innerHTML);
         books.push(newBook);
-        createElement(yesNo);
+        localStorage.setItem('myBook', JSON.stringify(books));
+        createElement(newBook);
     }
     else alert('Fill out the input section');
     
+    for(let i = 0; i < read.length; i++){
+        read[i].style.background = '';
+        read[i].style.color = '';
+    }
 })
 
+restore();
